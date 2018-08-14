9df3b8b6b74722e8c75bf69bcbb0129c811e8abd
Kevin Shen <kevin_shen@hotmail.ca>
2018-08-14 17:00:35 -0400

    No spaces vv fast

--- memegetter.py
+++ memegetter.py
@@ -1,21 +1,18 @@
+#Author: Kevin Shen
 import requests
 import json
 from selenium import webdriver
 import time
-
 USER = ""
 PASS = ""
 CHAT = ''
-
 r = requests.get('https://www.reddit.com/r/dankmemes/top/.json?sort=top&t=day', headers = {'User-agent': 'Chrome'})
-
 theJSON = json.loads(r.text)
 links = []
 for i in range (1, 20):
     link = theJSON["data"]["children"][i]["data"]["preview"]["images"][0]["source"]["url"]
     #print(link)
     links.append(link)
-
 i = "meme"
 counter = 0
 for link in links: 
@@ -26,7 +23,6 @@ for link in links:
     if response.status_code == 200:
         with open(name, 'wb') as f:
             f.write(response.content)
-
 chrome_options = webdriver.ChromeOptions()
 prefs = {"profile.default_content_setting_values.notifications" : 2}
 chrome_options.add_experimental_option("prefs",prefs)
@@ -34,24 +30,16 @@ chrome_options.add_experimental_option("prefs",prefs)
 browser=webdriver.Chrome(chrome_options=chrome_options)
 # Open chat
 browser.get(CHAT)
-
 browser.find_element_by_id("email").send_keys(USER)
 browser.find_element_by_id("pass").send_keys(PASS)
-
 login = browser.find_element_by_id("loginbutton").click()
-
 imageinp = browser.find_element_by_class_name("_260t").send_keys("/path/to/meme")
 buttons = browser.find_element_by_class_name("_5rpb")
 sender = browser.find_element_by_class_name("notranslate")
 print(type(sender))
 sender.send_keys(u'\ue007')
-
 start = time.time()
-
 end = time.time()
 while ((end-start) < 5):
     end = time.time()
-
-
-
 browser.close()
